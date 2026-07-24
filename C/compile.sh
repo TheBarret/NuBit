93594b74d66c7602b2e1f3604fbbcb79cef362fd
Zephyroku <format.e.drive@gmail.com>
2026-07-24 13:16:45 +0200

    Optimize compilation commands in compile.sh
    Updated compilation commands to use optimization flags for certain files and reorganized the build process.

--- C/compile.sh
+++ C/compile.sh
@@ -5,33 +5,25 @@ echo "Compiling NuBit runtime..."
 # Clean old files
 rm -f *.o nubit
 
-# Compile with optimizations
-#echo "gates..."
-#gcc -O3 -march=native -Wall -Wextra -c gates.c -o gates.o -DNDEBUG
-#echo "adder..."
-#gcc -O3 -march=native -Wall -Wextra -c adder.c -o adder.o -DNDEBUG
-#echo "alu16..."
-#gcc -O3 -march=native -Wall -Wextra -c alu.c -o alu.o -DNDEBUG
-#echo "cpu16..."
-#gcc -O3 -march=native -Wall -Wextra -c cpu.c -o cpu.o -DNDEBUG
-#echo "assembler..."
-#gcc -o nb-asm nb-asm.c
-#echo "bus..."
-#gcc -O2 -Wall -Wextra -c bus.c -o bus.o -DNDEBUG
-#echo "entrypoint..."
-#gcc -O3 -march=native -Wall -Wextra main.c gates.o adder.o alu.o cpu.o -o nubit -lm -DNDEBUG
-
-gcc -O2 -Wall -Wextra -c gates.c -o gates.o -DNDEBUG
-gcc -O2 -Wall -Wextra -c adder.c -o adder.o -DNDEBUG
-gcc -O2 -Wall -Wextra -c alu.c -o alu.o -DNDEBUG
-gcc -O2 -Wall -Wextra -c cpu.c -o cpu.o -DNDEBUG
+# Gates, adder, alu, cpu, bus
+gcc -O3 -march=native -Wall -Wextra -c gates.c -o gates.o -DNDEBUG
+gcc -O3 -march=native -Wall -Wextra -c adder.c -o adder.o -DNDEBUG
+gcc -O3 -march=native -Wall -Wextra -c alu.c -o alu.o -DNDEBUG
+gcc -O3 -march=native -Wall -Wextra -c cpu.c -o cpu.o -DNDEBUG
 gcc -O2 -Wall -Wextra -c bus.c -o bus.o -DNDEBUG
-gcc -O2 -Wall -Wextra -c main.c -o main.o -DNDEBUG
 
-gcc -o nubit gates.o adder.o alu.o cpu.o bus.o main.o -DNDEBUG
+# Neural Associative Memory (NAM)
+gcc -O3 -march=native -Wall -Wextra -c nam_io.c -o nam_io.o -DNDEBUG
+gcc -O3 -march=native -Wall -Wextra -c nam.c -o nam.o -DNDEBUG
+
+# Application
+gcc -O2 -Wall -Wextra -c main.c -o main.o -DNDEBUG
+gcc -o nubit gates.o adder.o alu.o nam_io.o nam.o cpu.o bus.o main.o -DNDEBUG
+
+# Assembler
 gcc -o nb-asm nb-asm.c
 
-
+# inform
 if [ $? -eq 0 ]; then
     echo "Build successful!"
 else
